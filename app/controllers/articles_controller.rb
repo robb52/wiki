1a258b28bb606297e1d255a4e8dbfa9eb6de2275
Rob Brown <brown4homes@yahoo.com>
2019-02-22 16:15:33 -0800

    Add articles controller, views, and model

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    parmas.require(:article).permit(:title, :content)
+  end
+
+  def find_articles
+
+  end
+
+end
